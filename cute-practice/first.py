ac4df7033b071cd5987f30f366e70292e70675bd
Ubuntu <ubuntu@ip-172-31-6-188.ap-northeast-1.compute.internal>
2026-04-15 23:08:29 +0000

    add vectorised addition

--- cute-practice/first.py
+++ cute-practice/first.py
@@ -2,6 +2,8 @@
 
 import cutlass
 import cutlass.cute as cute
+from cutlass.cute.runtime import from_dlpack
+import torch
 
 @cute.kernel
 def hello_world():
@@ -10,16 +12,96 @@ def hello_world():
     if tidx == 0:
         cute.printf("Hello World!")
 
+@cute.kernel
+def printer(a : cutlass.Int32, b: cutlass.Constexpr[int]):
+    print('static printing a: ', a)
+    print('static printing b: ', b)
+    cute.printf("dynamic printing a: {}", a)
+    cute.printf("static printing b: {}", b)
+
+@cute.kernel
+def adder(one: cute.Tensor, two: cute.Tensor, 
+            three: cute.Tensor, size: cutlass.Int32):
+
+    tidx, _, _ = cute.arch.thread_idx()
+    bidx, _, _ = cute.arch.block_idx()
+    gidx, _, _ = cute.arch.block_dim()
+
+    global_idx = gidx*bidx + tidx;
+
+    print(f'one layout: {one}')
+    print(f'two layout: {two}')
+
+    x = tidx % size
+    y = tidx // size
+
+    fragA = one[(None, global_idx)].load()
+    fragB = two[(None, global_idx)].load()
+
+    three[(None, global_idx)] = fragA + fragB
 
 @cute.jit
-def wrapper():
+def wrapper(a_: cute.Tensor, b_: cute.Tensor, answer_: cute.Tensor):
     cutlass.cuda.initialize_cuda_context()
-    hello_kernel().launch(
+    hello_world().launch(
             grid=(1,1,1),block=(1,1,1)
+            )
+
+    printer(cutlass.Int32(10), 12).launch(
+            grid=(1,1,1), block=(1,1,1)
+            )
+
+    ## Now let's do some fun things. ##
+
+    ## Here is a sample vectorized addition kernel. ##
+
+    gA = cute.zipped_divide(a_, (1, 8)) ## Layout here: (1, 8),(16, 2) : (8, 1),(16,8). or are the strides (16, 1) (16, 8) ? I think it's the former.
+    gB = cute.zipped_divide(b_, (1, 8)) ## Same layout as ^.
+    answer = cute.zipped_divide(answer_, (1, 8)) ## Same layout as ^.
+
+    thread_cnt = 16
+
+    size = torch.numel(one)
+
+    ## now how do we compute grid size? [one.shape(0) // (thread_cnt * mode[0].size())] + 1
+
+    ## Alternatively, we can also do: [cute.size(gC, mode=[1]) // thread_cnt + 1] --> How does this make sense?
+
+    ## Then, how do we index within the tensor? use both block and thread-idxs.
+
+    adder(gA, gB, answer, cutlass.Int32(one.shape[0])).launch(
+            grid=((size // (thread_cnt * cute.size(gA, mode=[0])) + 1),1,1),
+            block=(thread_cnt, 1, 1)
             )
 
 
 if __name__ == '__main__':
-    comp_func = cute.compile(wrapper)
+    one = torch.randn(1024, 1024, dtype=torch.bfloat16, device="cuda")
+    two = torch.randn(1024, 1024, dtype=torch.bfloat16, device="cuda")
+    answer = torch.zeros_like(one)
 
-    comp_func()
+    a_ = from_dlpack(one)
+    b_ = from_dlpack(two)
+    answer_ = from_dlpack(answer)
+
+    comp_func = cute.compile(wrapper, a_, b_, answer_)
+    for _ in range(10):
+        comp_func(a_, b_, answer_)
+
+    start = torch.cuda.Event(enable_timing=True)
+    end = torch.cuda.Event(enable_timing=True)
+
+    ## Reset data to remove l2 cache pollution. ##
+    one = torch.randn(1024, 1024, dtype=torch.bfloat16, device="cuda")
+    two = torch.randn(1024, 1024, dtype=torch.bfloat16, device="cuda")
+    answer = torch.zeros_like(one)
+
+    a_ = from_dlpack(one)
+    b_ = from_dlpack(two)
+    answer_ = from_dlpack(answer)
+
+    start.record()
+    comp_func(a_, b_, answer_)
+    end.record()
+
+    print(f'is correct: {torch.allclose(one+two, answer)}, cute-dsl throughput: {torch.numel(one) / start.elapsed_time(end) / 1e12} FLOP/s')
